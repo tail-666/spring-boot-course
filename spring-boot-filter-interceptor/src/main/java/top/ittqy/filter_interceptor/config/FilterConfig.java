b792e74736418ac4f0a0d646753438ba7a694f08
codertqy <2725804977@qq.com>
2025-09-26 16:56:43 +0800

    feat：实现限制分流接口

--- spring-boot-filter-interceptor/src/main/java/top/ittqy/filter_interceptor/config/FilterConfig.java
+++ spring-boot-filter-interceptor/src/main/java/top/ittqy/filter_interceptor/config/FilterConfig.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import top.ittqy.filter_interceptor.filter.MyFilter;
-import top.ittqy.filter_interceptor.filter.YourFilter;
+import top.ittqy.filter_interceptor.filter.RateLimitFilter;
 
 /**
  * @Author: 27258
@@ -14,24 +13,33 @@ import top.ittqy.filter_interceptor.filter.YourFilter;
 @Configuration
 @AllArgsConstructor
 public class FilterConfig {
-    private final MyFilter myFilter;
-    private final YourFilter yourFilter;
+    // private final MyFilter myFilter;
+    // private final YourFilter yourFilter;
+    private final RateLimitFilter rateLimitFilter;
 
+    // @Bean
+    // public FilterRegistrationBean<MyFilter> MyFilterRegistrationBean() {
+    //     FilterRegistrationBean<MyFilter> registrationBean = new FilterRegistrationBean<>();
+    //     registrationBean.setFilter(myFilter);
+    //     registrationBean.setOrder(3);
+    //     registrationBean.addUrlPatterns("/api/test");
+    //     return registrationBean;
+    // }
+    //
+    // @Bean
+    // public FilterRegistrationBean<YourFilter> YourFilterRegistrationBean() {
+    //     FilterRegistrationBean<YourFilter> registrationBean = new FilterRegistrationBean<>();
+    //     registrationBean.setFilter(yourFilter);
+    //     registrationBean.setOrder(1);
+    //     registrationBean.addUrlPatterns("/api/test");
+    //     return registrationBean;
+    // }
     @Bean
-    public FilterRegistrationBean<MyFilter> MyFilterRegistrationBean() {
-        FilterRegistrationBean<MyFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(myFilter);
-        registrationBean.setOrder(3);
-        registrationBean.addUrlPatterns("/api/test");
-        return registrationBean;
-    }
-
-    @Bean
-    public FilterRegistrationBean<YourFilter> YourFilterRegistrationBean() {
-        FilterRegistrationBean<YourFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(yourFilter);
+    public FilterRegistrationBean<RateLimitFilter> RateLimitFilterRegistrationBean() {
+        FilterRegistrationBean<RateLimitFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(rateLimitFilter);
         registrationBean.setOrder(1);
-        registrationBean.addUrlPatterns("/api/test");
+        registrationBean.addUrlPatterns("/api/pay/*");
         return registrationBean;
     }
 }
--- spring-boot-filter-interceptor/src/main/java/top/ittqy/filter_interceptor/controller/TestController.java
+++ spring-boot-filter-interceptor/src/main/java/top/ittqy/filter_interceptor/controller/TestController.java
@@ -2,6 +2,7 @@ package top.ittqy.filter_interceptor.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.ittqy.filter_interceptor.result.Result;
@@ -14,12 +15,27 @@ import top.ittqy.filter_interceptor.result.Result;
 @Slf4j
 @RequestMapping("/api")
 public class TestController {
+    /**
+     * 测试限流支付
+     * @param id
+     * @return
+     */
+    @GetMapping("/pay/{id}")
+    public Result<String> pay(@PathVariable int id) {
+        log.info("开始支付");
+        return Result.ok("支付成功，订单号为：" + id);
+    }
+
+    /**
+     * 测试过滤器逻辑顺序
+     * @return
+     */
     @GetMapping("/test")
     public Result<String> test() {
         log.info("进入 TestController");
         try {
             Thread.sleep(3000);
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         return Result.ok("hello world");
